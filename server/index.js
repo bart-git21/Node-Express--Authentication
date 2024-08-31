6de2282441a6becdb83dffd40ef953313e857257
gitName <bartk@list.ru>
2024-08-31 19:53:30 +0300

    feat: add cors package

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 import express from "express";
 import router from "./router/index.js";
 import "dotenv/config";
+import cors from "cors";
 
 const app = express();
 const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || 7000;
+const corsOption = {
+    origin: process.env.URL, // allow the 'https://client-domain.com' to send requests
+    credentials: true, // allow the browser to include cookies & auth.headers
+}
 
 app.use("/", router);
+app.use(cors(corsOption));
 
 try {
   app.listen(PORT, HOST, () => {
