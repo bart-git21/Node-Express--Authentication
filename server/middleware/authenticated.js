df500a81c449095e125734ac17b13162b6a7bfee
gitName <bartk@list.ru>
2025-01-27 21:56:02 +0300

    refactor: clarify response message

--- server/middleware/authenticated.js
+++ server/middleware/authenticated.js
@@ -3,19 +3,14 @@ import invalidRefreshTokensList from "../../db/invalidToken.js";
 
 const authenticateToken = (req, res, next) => {
   try {
-    // const refreshToken = req.cookies.refreshToken;
-    // if (!refreshToken) res.status(401).send("Unauthorized");
-    // if (invalidRefreshTokensList.includes(refreshToken))
-    //   res.status(401).send("Refresh token is invalid");
-
     const authorizationHeaders = req.headers.authorization;
     const accessToken =
       authorizationHeaders && authorizationHeaders.split(" ")[1];
-    if (!accessToken) res.status(401).send("Access token is not found");
+    if (!accessToken) res.status(401).send("Unauthorized");
+    //   client fetch("/auth") and get new access token
 
     jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
-      if (err)
-        return res.status(403).send("Middleware. Access token is invalid");
+      if (err) res.status(401).send("Access token is invalid");
       req.user = payload;
       next();
     });
