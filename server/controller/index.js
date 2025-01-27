f411f816f0db601a79838498b0517d7f8d55e433
gitName <bartk@list.ru>
2025-01-27 21:59:24 +0300

    refactor: add error handlers; add client side comments

--- server/controller/index.js
+++ server/controller/index.js
@@ -15,13 +15,31 @@ function generateRefreshToken(payload) {
 }
 
 const controller = {
-  login(req, res) {
+  register(req, res) {
+    //   client fetch body {login, password}
     try {
-      // TODO. check if fields are not empty. 422 status
-      // TODO. check if user exists and password matches. 401 status
-      const activeUser = users[2];
-      const accessToken = generateAccessToken(activeUser);
-      const refreshToken = generateRefreshToken(activeUser);
+      const { login, password } = req.body;
+
+      if (!login || !password) res.status(422).send("Data is empty or invalid");
+      if (users.find((e) => e.login === login))
+        res.status(400).send("The name/email is already taken");
+
+      users.push({ id: 3, login, password, role: "user" });
+      res.status(201);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  login(req, res) {
+    //   client fetch body {login, password}
+    try {
+      const { login, password } = req.body;
+      const user = users.find(
+        (e) => e.login === login && e.password === password
+      );
+      if (!user) res.status(400).send("User not found");
+      const accessToken = generateAccessToken(user);
+      const refreshToken = generateRefreshToken(user);
       refreshTokens.push(refreshToken);
       console.log("refreshToken", refreshToken);
       console.log("accessToken", accessToken);
@@ -31,68 +49,50 @@ const controller = {
         sameSite: "Strict",
       });
       res.status(200).json({ accessToken });
+      //   client localStorage.setItem("accessToken", accessToken);
     } catch (err) {
       if (err instanceof Error) {
         console.log(err.message);
       }
     }
   },
-  refresh(req, res) {
+  token(req, res) {
     const token = req.cookies.refreshToken;
     if (!token)
-      return res
-        .status(401)
-        .send("Refresh token is not found. You need to sign in");
+      res.status(401).send("Refresh token is not found. You need to sign in");
+    if (invalidRefreshTokensList.includes(token))
+      res.status(401).send("Refresh token is invalid. You need to sign in");
     if (!refreshTokens.includes(token))
-      return res
-        .status(403)
-        .send("Refresh token is expired.You need to sign in");
+      res.status(403).send("Refresh token is expired. You need to sign in");
 
     jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, payload) => {
-      if (err) return res.status(403).send("Refresh token is invalid");
+      if (err) res.status(403).send("Refresh token is invalid");
       const accessToken = generateAccessToken(payload);
-      res.json({ accessToken });
+      res.status(200).json({ accessToken });
+      //   client localStorage.setItem("accessToken", accessToken);
+      //   client again fetch("/users", body:{id:1});
     });
   },
   logout(req, res) {
+    //   client localStorage.removeItem("accessToken");
     try {
       const refreshToken = req.cookies.refreshToken;
       const index = refreshTokens.findIndex((e) => e === refreshToken);
       refreshTokens.splice(index, 1);
       invalidRefreshTokensList.push(refreshToken);
-      res
-        .status(204)
-        .clearCookie("refreshToken");
+      res.status(204).clearCookie("refreshToken");
     } catch (err) {
-      if (err instanceof Error) {
-        console.log(err.message);
-      }
+      if (err instanceof Error) console.log(err.message);
     }
   },
-  register(req, res) {
-    try {
-      const newUser = {
-        id: 3,
-        login: "john",
-        password: "789",
-        role: "moderator",
-      };
-      // TODO. check if fields are not empty. 422 status
-      // TODO. check if user exists. 400 status
-      // TODO. check if email/login exists. 400 status
-
-      users.push(newUser);
-      return res.status(201).json(newUser);
-    } catch (err) {
-      console.log(err);
-    }
-  },
+  //   user
   getUser(req, res) {
+    //   client fetch("/users", headers: Authorization: Bearer <access_token>, body:{id:1});
     try {
-      const user = users.find((e) => e.id === req.user.id);
-      res.status(200).json(user);
+      const id = users.find((e) => e.id === req.user.id).id;
+      res.status(200).json("some data");
     } catch (err) {
-      return console.error(err);
+      if (err instanceof Error) console.log(err.message);
     }
   },
   admin(req, res) {
