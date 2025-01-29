39d9bc8c2e5e7f269227a2a754eb8058c0e647b7
gitName <bartk@list.ru>
2025-01-29 21:32:28 +0300

    docs: use 2 spaces to indent for better readability

--- server/index.js
+++ server/index.js
@@ -8,17 +8,19 @@ const app = express();
 const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || 7000;
 const corsOption = {
-    origin: process.env.URL, // allow the 'https://client-domain.com' to send requests
-    credentials: true, // allow the browser to include cookies & auth.headers
-}
+  origin: process.env.URL, // allow the 'https://client-domain.com' to send requests
+  credentials: true, // allow the browser to include cookies & auth.headers
+};
 
 app.use(cors(corsOption));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/", router);
 
-app.get("/", (req, res)=>{res.status(200).send("Authentication application")})
+app.get("/", (req, res) => {
+  res.status(200).send("Authentication application");
+});
 
 try {
   app.listen(PORT, HOST, () => {
